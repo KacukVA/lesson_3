9b46979733903d827eeeaf8d2b13571662e5e712
KacukVA <kacukva@gmail.com>
2022-09-21 01:10:13 +0300

    Released functions: requirements, generate_users, space with Flask framework

--- main.py
+++ main.py
@@ -1,2 +1,47 @@
+from flask import Flask, render_template, request
+from faker import Faker
+import requests
+import random
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/requirements/', methods=['get'])
+def requirements():
+    context = {}
+    with open(generate_if_not_exists('requirements.txt'), 'r') as f:
+        context['content'] = f.read().split()
+    return render_template('requirements.html', **context)
+
+
+@app.route('/generate-users/', methods=['get', 'post'])
+def generate_users():
+    context = {}
+    if request.method == 'POST':
+        emails_amount = int(request.form['emails_amount'])
+        domains = ['@gmail.com', '@yahoo.com', '@ukr.net', '@outlook.com']
+        fake = Faker()
+        emails = [fake.name().replace(' ', '.').lower() + random.choice(domains) for _ in range(emails_amount)]
+        context['emails'] = emails
+    return render_template('generate-users.html', **context)
+
+
+@app.route('/space/', methods=['get'])
+def space():
+    context = {'astro_number': 0}
+    r = requests.get('http://api.open-notify.org/astros.json')
+    if r.status_code == 200:
+        context['astro_number'] = len(r.json()["people"])
+    return render_template('space.html', **context)
+
+
+def generate_if_not_exists(file: str) -> str:
+    if not os.path.exists(file):
+        os.system('pip freeze > requirements.txt')
+    return 'requirements.txt'
+
+
 if __name__ == '__main__':
-    ...
+    app.run()
